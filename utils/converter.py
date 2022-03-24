31193157a55a28f7c26a01dcff9ddcdc08a11fb1
askazin <kazin.aleks@gmail.com>
2022-03-24 15:30:30 +0300

    update .gitignore & add some .py

---
+++ utils/converter.py
@@ -0,0 +1,29 @@
+import json
+
+infile = "*.txt"
+outfile = "*.json"
+
+dict1 = {}
+fields = ["datetime", "problemtype", "productname", "url", "comment"]
+with open(infile, 'r', encoding='utf-8') as fh:
+    l = 1
+
+    for line in fh:
+        description = list(line.strip().split('\t', 4))
+
+        print(description)
+
+        sno = 'field' + str(l)
+
+        i = 0
+        dict2 = {}
+        while i < len(fields):
+            dict2[fields[i]] = description[i]
+            i = i + 1
+
+        dict1[sno] = dict2
+        l = l + 1
+
+out_file = open(outfile, 'w', encoding='utf-8')
+json.dump(dict1, out_file, indent='\t')
+out_file.close()
