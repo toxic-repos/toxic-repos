020e3975520f5bd32b2ba2856d2375632e6bb3af
askazin <kazin.aleks@gmail.com>
2022-03-25 12:45:56 +0300

    repository review

--- utils/converter.py
+++ utils/converter.py
@@ -1,11 +1,13 @@
+import sqlite3
 import json
 
-infile = "*.txt"
-outfile = "*.json"
+infile = "../toxic-repos.txt"
+outfile = "../toxic-repos.json"
 
 dict1 = {}
 fields = ["datetime", "problemtype", "productname", "url", "comment"]
 with open(infile, 'r', encoding='utf-8') as fh:
+    print("Читаем txt файл")
     l = 1
 
     for line in fh:
@@ -13,7 +15,7 @@ with open(infile, 'r', encoding='utf-8') as fh:
 
         print(description)
 
-        sno = 'field' + str(l)
+        sno = str(l)
 
         i = 0
         dict2 = {}
@@ -26,4 +28,26 @@ with open(infile, 'r', encoding='utf-8') as fh:
 
 out_file = open(outfile, 'w', encoding='utf-8')
 json.dump(dict1, out_file, indent='\t')
+print("Пишем json файл")
 out_file.close()
+
+traffic = json.load(open('../toxic-repos.json'))
+print("Читаем json файл")
+
+try:
+    db = sqlite3.connect("../sqlite/toxic-repos.sqlite3")
+    print("Соединяемся с SQLite базой")
+    query = "insert into repos values (?, ?, ?, ?, ?, ?)"
+    columns = ['datetime', 'problemtype', 'productname', 'url', 'comment']
+    for datetime, data in traffic.items():
+        keys = (datetime,) + tuple(data[c] for c in columns)
+        c = db.cursor()
+        c.execute(query, keys)
+except sqlite3.Error as error:
+    print("Ошибка при работе с SQLite базой", error)
+finally:
+    if db:
+        db.commit()
+        print("Записи успешно вставлены в таблицу.")
+        db.close()
+        print("Соединение с SQLite закрыто")
