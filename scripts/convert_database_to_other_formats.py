d1110e40a8157bfe875a2ddf5df3094e7ce4a482
Stravnik <admin@zerobot.ru>
2022-04-16 08:42:51 +0300

    clean up & fixes(PR #1)

--- scripts/convert_database_to_other_formats.py
+++ scripts/convert_database_to_other_formats.py
@@ -5,7 +5,6 @@ import sqlite3
 import json
 import csv
 from pathlib import Path
-from dataclasses import dataclass
 
 DATABASE_PATH = Path("../data/sqlite/toxic-repos.sqlite3")
 
@@ -17,31 +16,28 @@ def main() -> None:
     connection = sqlite3.connect(DATABASE_PATH)
     cursor = connection.cursor()
 
-    database_data: t.List[t.Dict[str, t.Any]] = []
+    execute_result = cursor.execute("SELECT * FROM repos ORDER BY problem_type")
 
-    for row in cursor.execute("SELECT * FROM repos ORDER BY problem_type"):
-        database_data.append(
-            dict(
-                datetime=row[1],
-                problem_type=row[2],
-                name=row[3],
-                commit_link=row[4],
-                description=row[5],
-            ),
-        )
+    column_datas: t.List[t.Tuple[t.Any, ...]] = list(execute_result)  # type: ignore
+    row_names = list(map(lambda x: x[0], cursor.description))
 
     # To json
+    json_data = [
+        {
+            row_names[index]: data
+            for index, data in enumerate(column_data)
+        }
+        for column_data in column_datas
+    ]
     with open(JSON_OUTFILE_PATH, "w") as fp:
-        json.dump(database_data, fp, ensure_ascii=False, indent=4)
+        json.dump(json_data, fp, ensure_ascii=False, indent=4)
 
     # To csv
     with open(CSV_OUTFILE_PATH, "w", newline='') as fp:
         csv_writer = csv.writer(fp)
-        csv_writer.writerow(
-            ["datetime", "problem_type", "name", "commit_link", "description"],
-        )
-        for row in database_data:
-            csv_writer.writerow(row.values())
+        csv_writer.writerow(row_names)
+        for column_data in column_datas:
+            csv_writer.writerow(column_data)
 
 
 if __name__ == "__main__":
